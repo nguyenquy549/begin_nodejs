11712d9bb0607edf6bf5a70903a36f75b7853a4d
nguyenquy549 <nguyenquy549@gmail.com>
2020-10-01 17:10:20 +0700

    1

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-const app = express();
-const port = 3000;
-app.get("/a", (req, res) => {
-  res.send("Hello World!");
-});
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
